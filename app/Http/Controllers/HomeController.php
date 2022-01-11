00159e3b6a4e4eff5a0a43223f8694faa6f15930
Fernando Mastropietro <femastro@gmail.com>
2022-01-11 14:30:47 -0300

    se agrego la conexion en el metodo index() del HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = DB::table('posts')->get();
+        $states = DB::table('provincias')->get();
+        return view('home', compact('posts'), compact('states'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,42 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Lista de Posteos') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Lista de Posteos') }}</div>
 
-                <div class="card-body">
-                    <table class="table table-hover">
-                        <thead>
-                          <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Provincia</th>
-                            <th scope="col">Titulo</th>
-                            <th scope="col">Descripción</th>
-                            <th scope="col">Acción</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Cordoba</td>
-                            <td>Fiesta de la Cerbeza</td>
-                            <td>Column content Fiesta de la Cerbeza</td>
-                            <th scope="col">
-                                <a class="btn btn-primary" href="">Edit</a>
-                                <a class="btn btn-danger" href="">Borra</a>
-                            </th>
-                          </tr>
-                        </tbody>
-                    </table>
+                    <div class="card-body">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Provincia</th>
+                                    <th scope="col">Titulo</th>
+                                    <th scope="col">Descripción</th>
+                                    <th scope="col">Url Imágen</th>
+                                    <th scope="col">Acción</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($posts as $post)
+                                    <tr>
+                                        <th scope="row">{{ $post->id }}</th>
+                                        <td>
+                                            @foreach ($states as $state)
+                                                @if ( $state->id == $post->state )
+                                                    {{ $state->state }}
+                                                @endif
+                                            @endforeach
+                                        </td>
+                                        <td>{{ $post->title }}</td>
+                                        <td>{{ $post->description }}</td>
+                                        <td>{{ $post->imagen }}</td>
+                                        <th scope="col">
+                                            <a class="btn btn-primary" href="">Edit</a>
+                                            <a class="btn btn-danger" href="">Borra</a>
+                                        </th>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
+
+
+
         </div>
-
-
-
     </div>
-</div>
 @endsection
